6ae3ced764de7135924896079a3d8842615637f5
Mark Santos <msantos1996@pointloma.edu>
2016-10-06 20:35:51 -0700

    Initial commit

---
+++ Assets/ShellSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellSpawner : MonoBehaviour {
+    public GameObject myObject;
+    public Vector3 spawnvec;
+    public float Timer;
+    public float spawnnumber;
+
+    void Awake()
+    {
+         Timer = Time.time + 2;
+    }
+
+    void Update()
+    {
+
+
+        spawnvec = new Vector3(Random.Range(-40f, 40f), 40, Random.Range(-40f, 40f));
+        if (Timer < Time.time)
+        {
+            Instantiate(myObject, spawnvec, Quaternion.Euler(90f, 0f, 0f));
+            Timer = Time.time + 3;
+        } 
+    }
+}
